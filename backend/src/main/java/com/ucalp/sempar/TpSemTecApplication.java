676a65717710c5baf09fb38262ce79a9d39689a9
fpaulettich <fpaulettich@gmail.com>
2021-04-24 16:50:26 -0300

    primer código

---
+++ backend/src/main/java/com/ucalp/sempar/TpSemTecApplication.java
@@ -0,0 +1,13 @@
+package com.ucalp.sempar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpSemTecApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(TpSemTecApplication.class, args);
+    }
+
+}
